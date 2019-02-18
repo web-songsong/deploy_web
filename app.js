2e6fbcc721a55894edb4b487eeb67328f4638933
charmingsong <web_songsong@163.com>
2019-02-18 18:02:12 +0800

    111

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const Koa = require('koa')
 const app = new Koa()
 const router = require('./server/index.js')
-const bodyParser = require('koa-bodyparser')
-app.use(bodyParser())
 app.use(router.routes())
 app.listen(3000)
 
--- server/index.js
+++ server/index.js
@@ -1,34 +1,34 @@
 const router = require('koa-router')()
 const path = require('path')
+const exists = require('fs').existsSync
 const { atten, log, success, fatal } = require('../utils/logger.js')
 const exec = require('util').promisify(require('child_process').exec)
 const ora = require('ora')
 router.post('/deploy/:projectName', async ctx => {
-  // atten()
-  // if (!ctx.header['user-agent'].includes('GitHub-Hookshot')) {
-  //   fatal('请求来源未知：\n     %s', ctx.header['user-agent'])
-  //   return (ctx.status = 401)
-  // }
-  // const { commits } = ctx.request.body
-  console.log(ctx.request.body)
-  // const dirHome = path.join(__dirname, `../../${ctx.params.projectName}`)
-  // const shellOperation = `git pull && yarn && yarn deploy`
-  // const spinner = ora('work').start()
+  atten()
+  if (!ctx.header['user-agent'].includes('GitHub-Hookshot')) {
+    fatal('请求来源未知：\n     %s', ctx.header['user-agent'])
+    return (ctx.status = 401)
+  }
+  const dirHome = path.join(__dirname, `../../${ctx.params.projectName}`)
+  const shellOperation = `git pull && yarn && yarn deploy`
+  const spinner = ora('work').start()
 
-  // ctx.body = await exec(shellOperation, { cwd: dirHome, shell: '/bin/zsh' })
-  //   .then(res => {
-  //     spinner.succeed()
-  //     success(shellOperation)
-  //     return ` success : ${commits.message}  `
-  //   })
-  //   .catch(err => {
-  //     spinner.fail()
-  //     fatal(shellOperation)
-  //     log(`检查 ${dirHome} 文件`)
-  //     log(`检查  ${shellOperation} 命令`)
-  //     fatal(err)
-  //     return 'error'
-  //   })
+  ctx.body = exists(dirHome) ? '请求部署成功请稍后' : '确认项目是否存在'
+  await exec(shellOperation, { cwd: dirHome, shell: '/bin/zsh' })
+    .then(res => {
+      spinner.succeed()
+      success(shellOperation)
+      return ` success : ${c}  `
+    })
+    .catch(err => {
+      spinner.fail()
+      fatal(shellOperation)
+      log(`检查 ${dirHome} 文件`)
+      log(`检查  ${shellOperation} 命令`)
+      fatal(err)
+      return 'error'
+    })
 })
 
 module.exports = router
