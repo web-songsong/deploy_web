44ffa722cab185a62ce186e5b7d00b87861cddb0
charming song <web_songsong@163.com>
2019-09-23 17:19:36 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const Koa = require('koa')
 const app = new Koa()
 const router = require('./server/index.js')
 app.use(router.routes())
-app.listen(3000)
+app.listen(6666)
 
 console.log('server ok!')
